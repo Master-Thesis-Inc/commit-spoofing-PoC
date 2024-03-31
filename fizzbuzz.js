5d410e0c599e187e81e210bdd097a96c95956f16
friendly-bottolfsen <friendly.bottolfsen@gmail.com>
2024-04-01 01:57:42 +0200

    Implement FizzBuzz

---
+++ fizzbuzz.js
@@ -0,0 +1,16 @@
+const fizzBuzz = (max) => {
+  for (i = 0; i <= max; i++) {
+    let toPrint = "";
+
+    if (i % 3 == 0) {
+      toPrint += "Fizz";
+    }
+
+    if (i % 5 == 0) {
+      toPrint += "Buzz";
+    }
+    console.log(toPrint || i);
+  }
+};
+
+fizzBuzz(50);
